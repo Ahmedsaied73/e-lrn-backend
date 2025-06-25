b9dc0b66aeff388f3567e3e8a57e5e674aaa7ade
Ahmedsaied73 <ahmedosaied73@gmail.com>
2025-06-25 13:51:25 +0300

    Delete admin-panel directory

--- admin-panel/src/components/PlaceholderComponent.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const PlaceholderComponent: React.FC = () => {
-  return <div>Placeholder Component</div>;
-};
-
-export default PlaceholderComponent;
--- admin-panel/src/pages/PlaceholderPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const PlaceholderPage: React.FC = () => {
-  return <h1>Placeholder Page</h1>;
-};
-
-export default PlaceholderPage;
