162fd77661dc792f7fa8cbdbabeef1e4194c5ad0
ahmed <ahmedosaied73@gmail.com>
2026-09-05 09:33:11 +0300

    chore: remove dead getCookieConfig() from utils.js
    Never imported anywhere; cookie config comes from src/config/cookie.

--- src/utils.js
+++ src/utils.js
@@ -8,28 +8,7 @@ function createRefreshToken(payload, secret) {
     return createToken(payload, secret, '7d');
 }
 
-// Function to get cookie configuration based on environment
-function getCookieConfig(isRefreshToken = false) {
-    // Check if we're in production mode
-    const isProduction = process.env.NODE_ENV === 'production';
-    
-    // Base cookie configuration
-    const cookieConfig = {
-        httpOnly: true,
-        sameSite: 'strict',
-        // Only set secure: true in production
-        secure: isProduction,
-        // Set appropriate expiry time
-        maxAge: isRefreshToken 
-            ? 7 * 24 * 60 * 60 * 1000 // 7 days in milliseconds for refresh token
-            : 60 * 60 * 1000 // 1 hour in milliseconds for access token
-    };
-    
-    return cookieConfig;
-}
-
 module.exports = {
     createToken,
-    createRefreshToken,
-    getCookieConfig
+    createRefreshToken
 };
